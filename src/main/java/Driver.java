05af0a77a50cfacef95fefd899a526887b6ae970
faliaaufa <55497068+faliaaufa@users.noreply.github.com>
2022-06-01 16:07:53 +0700

    Revert "Update Driver.java"
    This reverts commit 815428ee0b69d8b23095fd965fa5d31b0d0e20e9.

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -4,17 +4,17 @@ public class Driver {
 
 	public static void main(String[] args) {
 		
-		Counter count = new Counter();
+		Counter counter = new Counter();
 		
-		System.out.println("Count:" + count.getCount());
+		System.out.println("Count:" + counter.getCount());
 		
 		counter.increment();
 		
-		System.out.println("Count:" + count.getCount());
+		System.out.println("Count:" + counter.getCount());
 		
 		counter.decrement();
 		
-		System.out.println("Count:" + count.getCount());
+		System.out.println("Count:" + counter.getCount());
 		
 	}
 
